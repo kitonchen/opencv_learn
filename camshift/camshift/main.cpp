96e2d94be68e95baf4714cc6c70220e256f71b72
kiton <1026687121@qq.com>
2016-01-18 13:46:20 +0800

    更新颜色追踪

--- camshift/camshift/main.cpp
+++ camshift/camshift/main.cpp
@@ -94,8 +94,24 @@ int main()
 				break;
 			}
 			frame.copyTo(image);
-			if (!paused) {
-
+			if (!paused) {//没有按暂停键
+				cvtColor(image, hsv, CV_BGR2HSV);//将该帧RGB空间转为HSV空间
+				if (trackObject) {//初始化为0，或者按完键盘的'c'建后也为0，鼠标单击后为-1
+					int _vmin = vmin, _vmax = vmax;
+					//inRange函数的功能是检查输入数组每个元素大小是否在2个给定数值之间。可以有多通道，mask保存0通道的最小值，就是h分量
+					//这里利用了HSV的3个通道，比较H，0~180，S,Smin~256,V,min(vmin,vmax),max(vmin,vmax)如果3个通道都在对应的范围内，则msak对应的那个点的值为1（0XFF），否则为0（0X00）
+					inRange(hsv, Scalar(0, smin, min(_vmin, _vmax)), Scalar(180, 256, max(_vmin, _vmax)), mask);	
+					int ch[] = { 0,0 };
+					hue.create(hsv.size(), hsv.depth());//HUE初始化为一个与HSV大小深度一样的矩阵，色调的度量是用角度表示的，红绿蓝之间相差120度，反色相差180度
+					mixChannels(&hsv, 1, &hue, 1, ch, 1);//将HSV的第一个通道（也就是色调的数复制到HUE中，0索引数组）
+					if (trackObject < 0) {
+						//此处的构造函数ROI用的是Mat hue的矩阵头，且roi的数据指针指向hue,即共用相同的数据，select为其感兴趣的区域
+						Mat roi(hue, selection), maskroi(mask, selection);//mask保存的HSV的最小值
+						//calchist()函数第一个参数为输入矩阵序列，第2个参数表示输入的矩阵数目，第三个参数表示将被计算直方图维数通道的列表，第4个参数表示可选的掩码函数
+						//第5个参数表述输出直方图，第6个参数表示直方图的维数，第7个参数为每一维直方图数组的大小，第8个参数为每一维直方图bin的边界
+						calcHist(&roi, 1, 0, maskroi, hist, 1, &hsize, &phranges);//建行roi的0通道计算直方图并通过mask放入hist中，hsize为每一维直方图的大小
+					}
+				}
 			}
 		}
 	}
